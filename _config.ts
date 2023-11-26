0a3ab08d870b83c56876af6fb66f31163838ec48
JFG <joefullergray@gmail.com>
2023-11-26 20:05:21 +0000

    tech: Add date Lume plugin
    Required to generate a sitemap.

--- _config.ts
+++ _config.ts
@@ -1,8 +1,10 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
+import date from "lume/plugins/date.ts";
 
 const site = lume();
 site.use(postcss());
+site.use(date());
 site.ignore("README.md");
 site.copy("CNAME");
 site.copy("img");
